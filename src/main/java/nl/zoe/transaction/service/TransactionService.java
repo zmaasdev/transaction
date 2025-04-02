579628093de333e2b9031e9672f5151d627a647c
zmaasdev <neo.zoe.maas@gmail.com>
2025-04-02 16:51:42 +0200

    chore: Improved error handling

--- src/main/java/nl/zoe/transaction/service/TransactionService.java
+++ src/main/java/nl/zoe/transaction/service/TransactionService.java
@@ -1,35 +1,45 @@
 package nl.zoe.transaction.service;
 
+import io.micrometer.core.instrument.MeterRegistry;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import nl.zoe.transaction.event.TransactionEvent;
 import nl.zoe.transaction.model.Transaction;
 import nl.zoe.transaction.repository.TransactionRepository;
 import org.springframework.stereotype.Service;
 
-import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.Optional;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class TransactionService {
 
     private final TransactionRepository transactionRepository;
+    private final MeterRegistry meterRegistry;
 
     public void save(TransactionEvent transactionEvent) {
-        Optional<Transaction> optionalTransaction = transactionRepository.findByAccountId(transactionEvent.getAccountId());
-        if (optionalTransaction.isPresent()) {
-            Transaction transaction = optionalTransaction.get();
-            transaction.setBalance(transaction.getBalance().add(transactionEvent.getAmount()));
-            transaction.setCreatedAt(LocalDateTime.now());
-            transactionRepository.save(transaction);
-        } else {
-            Transaction transaction = new Transaction();
-            transaction.setAccountId(transactionEvent.getAccountId());
-            transaction.setAmount(transactionEvent.getAmount());
-            transaction.setBalance(transactionEvent.getAmount());
-            transaction.setCreatedAt(LocalDateTime.now());
-            transactionRepository.save(transaction);
+        try {
+            Optional<Transaction> optionalTransaction = transactionRepository.findByAccountId(transactionEvent.getAccountId());
+            if (optionalTransaction.isPresent()) {
+                Transaction transaction = optionalTransaction.get();
+                transaction.setBalance(transaction.getBalance().add(transactionEvent.getAmount()));
+                transaction.setCreatedAt(LocalDateTime.now());
+                transactionRepository.save(transaction);
+            } else {
+                Transaction transaction = new Transaction();
+                transaction.setAccountId(transactionEvent.getAccountId());
+                transaction.setAmount(transactionEvent.getAmount());
+                transaction.setBalance(transactionEvent.getAmount());
+                transaction.setCreatedAt(LocalDateTime.now());
+                transactionRepository.save(transaction);
+            }
+            meterRegistry.counter("transaction.count").increment();
+        } catch(Exception e) {
+            log.debug("Failed to save transaction", e);
+            meterRegistry.counter("transaction.error").increment();
+            throw new RuntimeException("Failed to save transaction", e);
         }
     }
 }
